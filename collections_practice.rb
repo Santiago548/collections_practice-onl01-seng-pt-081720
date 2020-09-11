70d8497533a87f8ae19a8dc7f8c6df2d533008c7
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-11 18:04:27 +0000

    Automatically backed up by Learn

---
+++ collections_practice.rb
@@ -0,0 +1,35 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  
+end
+
+def sort_array_char_count(array)
+  
+end
+
+def swap_elements(array)
+  
+end
+
+def reverse_array(array)
+  
+end
+
+def kesha_maker
+  
+end
+
+def find_a
+  
+end
+
+def sum_array
+  
+end
+
+def add_s
+  
+end
